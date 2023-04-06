5434b1a6282998318650380be2a0f0345d4ce893
ji yoon <wldbf11111@naver.com>
2023-04-06 13:48:10 +0900

    회원가입만들기 완료

---
+++ src/main/java/shop/mtcoding/bankapp/dto/user/LoginReqDto.java
@@ -0,0 +1,12 @@
+package shop.mtcoding.bankapp.dto.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginReqDto {
+    private Integer username;
+    private Integer password;
+
+}
