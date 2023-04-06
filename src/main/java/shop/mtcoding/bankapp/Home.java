de7d3fc1fa55bcd0956ed17be82390f362660553
ji yoon <wldbf11111@naver.com>
2023-04-06 10:05:17 +0900

    초기셋팅완료

---
+++ src/main/java/shop/mtcoding/bankapp/Home.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.bankapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Home {
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
